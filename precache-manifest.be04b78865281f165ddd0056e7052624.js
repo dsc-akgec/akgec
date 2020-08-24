85d2bfd96f3e5c5732a4607c1f18e1683231b48d
unknown <hiteshtripathi12345678@gmail.com>
2020-08-24 11:33:15 +0530

    Updates

---
+++ precache-manifest.be04b78865281f165ddd0056e7052624.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37eeb526faf0196ecf0311ff5ffc00b0",
+    "url": "/akgec/index.html"
+  },
+  {
+    "revision": "378868b97c3a86805efb",
+    "url": "/akgec/static/css/main.2c7124a4.chunk.css"
+  },
+  {
+    "revision": "5d042784f315a294356d",
+    "url": "/akgec/static/js/2.7a982b93.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/akgec/static/js/2.7a982b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378868b97c3a86805efb",
+    "url": "/akgec/static/js/main.c18b7ff2.chunk.js"
+  },
+  {
+    "revision": "6f27f6feb48dbf98d07d",
+    "url": "/akgec/static/js/runtime-main.492b7b1c.js"
+  },
+  {
+    "revision": "1f7d50937dbaa31ecd30839842757a2c",
+    "url": "/akgec/static/media/Calendar.1f7d5093.png"
+  },
+  {
+    "revision": "e3e4b249dd2a258e5ff6e128f7908bd7",
+    "url": "/akgec/static/media/aboutimg.e3e4b249.png"
+  },
+  {
+    "revision": "61123d7a4a66e2a24152bd0a2ac37481",
+    "url": "/akgec/static/media/footerLogo.61123d7a.png"
+  },
+  {
+    "revision": "3053a6705f357cc866e44a5e9a655a8b",
+    "url": "/akgec/static/media/homepage.3053a670.jpg"
+  },
+  {
+    "revision": "a7498669e87495e3f9998081aec77dc6",
+    "url": "/akgec/static/media/projectimg.a7498669.png"
+  },
+  {
+    "revision": "a4f650602fe8289399ceb1f5cd299d26",
+    "url": "/akgec/static/media/teamimg.a4f65060.png"
+  }
+]);
